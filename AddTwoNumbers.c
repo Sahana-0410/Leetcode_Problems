f809e09e3fe6281c6692bf677861932de8d9e709
Sahana-0410 <sahana04102005@gmail.com>
2026-04-17 22:19:21 +0530

    Create AddTwoNumbers.c

---
+++ AddTwoNumbers.c
@@ -0,0 +1,37 @@
+struct ListNode* createNode(int val) {
+    struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+    newNode->val = val;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    
+    struct ListNode dummy;
+    dummy.next = NULL;
+
+    struct ListNode* temp = &dummy;
+    int carry = 0;
+
+    while (l1 || l2 || carry) {
+
+        int sum = carry;
+
+        if (l1) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+
+        if (l2) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;
+
+        temp->next = createNode(sum % 10);
+        temp = temp->next;
+    }
+
+    return dummy.next;
+}
